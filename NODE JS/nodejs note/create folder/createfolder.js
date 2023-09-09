65623aa1f4f580881ebeb89857774253a696dc63
changer11 <nirajsingh4141@gmail.com>
2023-09-09 12:29:58 +0530

    project

--- NODE JS/nodejs note/create folder/createfolder.js
+++ NODE JS/nodejs note/create folder/createfolder.js
@@ -7,6 +7,7 @@ fs.writeFileSync("code.text","code step by step");
 fs.writeFileSync("hello.text","hello world");
 fs.writeFileSync("content.text","readable");
 // fs.read files process 
+fs.readFileSync()
 fs.readdir(dirname,(error,files)=>{
     files.forEach((item)=>{
         console.log(item);
